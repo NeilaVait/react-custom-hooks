e43cde1d451bb83d1e00da66b403a69bcd13c3aa
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-26 08:59:32 +0300

    use custom hook for up

--- src/components/Counter/UpCounter.jsx
+++ src/components/Counter/UpCounter.jsx
@@ -1,17 +1,8 @@
-import { useState, useEffect } from 'react';
 import Card from './../UI/Card';
+import useCounter from './../../hooks/use-counter';
 
 const UpCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const int1 = setInterval(() => {
-      setCounter((prevState) => prevState + 1);
-    }, 1000);
-    return () => {
-      clearInterval(int1);
-    };
-  }, []);
+  const counter = useCounter();
 
   return (
     <Card>
