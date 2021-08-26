89c2542ac09f91decaf21c3894647ab86feae85e
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-26 10:08:32 +0300

    use-http custom hook almost

---
+++ src/hooks/use-http..js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { dbUrl } from './../config';
+
+const useHttp = (requestConfig, successCallback) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        body: JSON.stringify(requestConfig.body),
+        headers: requestConfig.headers,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      successCallback(data);
+
+      console.log(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+};
+
+export default useHttp;
