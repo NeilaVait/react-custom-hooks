14dd7f3d8c59d8e50e49476e1154882bacfac3dd
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-26 08:44:19 +0300

    useeffect to up counter

--- src/components/Counter/UpCounter.jsx
+++ src/components/Counter/UpCounter.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './../UI/Card';
 
 const UpCounter = () => {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const int1 = setInterval(() => {
+      setCounter((prevState) => prevState + 1);
+    }, 1000);
+    return () => {
+      clearInterval(int1);
+    };
+  }, []);
+
   return (
     <Card>
       <h2>UpCounter</h2>
