5eed488be717328df70ebcd2c8ec8b40e07cfd74
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-26 08:48:51 +0300

    downCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import UpCounter from './components/Counter/UpCounter';
+import DownCounter from './components/Counter/DownCounter';
 
 function App() {
   return (
     <React.Fragment>
       <UpCounter />
+      <DownCounter />
     </React.Fragment>
   );
 }
