832bdd80b8a09b4b1dd5fbdf2baf1c0bb7136133
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-26 08:33:17 +0300

    card UI component

--- src/components/Counter/UpCounter.jsx
+++ src/components/Counter/UpCounter.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import Card from './../UI/Card';
+
 const UpCounter = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <>
+    <Card>
       <h2>UpCounter</h2>
-      <h2>0</h2>
-    </>
+      <h2>{counter}</h2>
+    </Card>
   );
 };
 
