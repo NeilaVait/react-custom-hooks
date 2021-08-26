29cb7674aea7d67bc313d223916c4705b4a786a6
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-26 09:41:37 +0300

    config.js dbUrl, feed dbUrl to fetch

---
+++ src/AppCounter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UpCounter from './components/Counter/UpCounter';
+import DownCounter from './components/Counter/DownCounter';
+
+function App() {
+  return (
+    <React.Fragment>
+      <UpCounter />
+      <DownCounter />
+    </React.Fragment>
+  );
+}
+
+export default App;
